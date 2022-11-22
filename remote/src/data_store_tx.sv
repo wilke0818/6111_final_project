f86e2939d0bba01b0fa4b5db5a1e353841da7388
Jordan Wilke <wilke18@mit.edu>
2022-11-21 20:54:25 -0500

    Added working data store

--- remote/src/data_store_tx.sv
+++ remote/src/data_store_tx.sv
@@ -13,20 +13,22 @@ module data_store_tx #(parameter N=2, parameter DATA_SIZE=16) (
   output logic [15:0] data_length //will be in bytes
 );
 
+  parameter CKSUM_COUNT_MAX  = 48/DATA_SIZE;
+
   logic [7:0] read_idx, write_idx;
-  logic [$clog2(16/N)-1:0] read_count;
+  logic [$clog2(DATA_SIZE/N)-1:0] read_count;
   logic [15:0] read_out;
   logic [8:0] bytes_written;
   logic [1:0] cycle_delay_start, cycle_delay_end;
   logic [16:0] sum;
-  logic [15:0] word1, word2, word3, word4;
+  logic [47:0] word;
 
   logic [3:0] position;
   logic [1:0] cksum_count;
 
   logic [DATA_SIZE-1:0] read_data;
 
-  assign data_cksum = ~sum;
+  assign data_cksum = ~ (sum+word[47:32]+word[31:16]+word[15:0] + ((sum+word[47:32]+word[31:16]+word[15:0])>>16));
   assign data_length = position > 8 ? (bytes_written<<1) + 2 : position > 0 ? (bytes_written<<1)+1 : bytes_written<<1;
 
   xilinx_true_dual_port_read_first_2_clock_ram #(
@@ -62,38 +64,25 @@ module data_store_tx #(parameter N=2, parameter DATA_SIZE=16) (
       sum <= 0;
 //      word_select <= 0;
       position <= 0;
-      word1 <= 0;
-      word2 <= 0;
-      word3 <= 0;
-      word4 <= 0;
       cksum_count <= 0;
+      word <= 0;
     end else begin
       if (axiiv) begin 
         write_idx <= write_idx + 1;
         bytes_written <= position + DATA_SIZE > 15 ? bytes_written + 1 : bytes_written;
-        
-        if (cksum_count == 0) begin
-           word1[15-position -: DATA_SIZE] <= axiid;
-           cksum_count <= cksum_count + 1;
-           position <= position + DATA_SIZE;
-           sum <= word1 + word2 + word3 + word4;
-           word2 <= 0;
-           word3 <= 0;
-           word4 <= 0;
-        end else if (cksum_count == 1) begin
-           word2[15-position -: DATA_SIZE] <= axiid;
-           cksum_count <= cksum_count + 1;
-           position <= position + DATA_SIZE;
-           sum <= sum[15:0] + sum[16];
-        end else if (cksum_count == 2) begin
-           word3[15-position -: DATA_SIZE] <= axiid;
-           cksum_count <= cksum_count + 1;
-           position <= position + DATA_SIZE;
+        position <= position + DATA_SIZE > 15 ? position + DATA_SIZE - 16 : position + DATA_SIZE;
+        if (cksum_count < CKSUM_COUNT_MAX - 1) begin
+          cksum_count <= cksum_count + 1;
+          word[47-cksum_count*DATA_SIZE -: DATA_SIZE] <= axiid;
         end else begin
-           word4[15-position -: DATA_SIZE] <= axiid;
-           cksum_count <= cksum_count + 1;
-           position <= position + DATA_SIZE;
-        end  
+          word <= 48'b0;
+          if (sum + word[47:32] + word[31:16] + word[15 -: 16-DATA_SIZE] + axiid > 17'h0_ffff) begin
+            sum <= sum + word[47:32] + word[31:16] + {word[15 -: 16-DATA_SIZE], axiid} + 1'b1;
+          end else begin
+            sum <= sum + word[47:32] + word[31:16] + {word[15 -: 16-DATA_SIZE], axiid};
+          end
+          cksum_count <= 0;
+        end
       end
     end
   end
@@ -104,28 +93,29 @@ module data_store_tx #(parameter N=2, parameter DATA_SIZE=16) (
       read_idx <= 0;
       axiov <= 0;
       read_count <= 0;
+      cycle_delay_start <= 0;
     end else begin
-      if (read_request) begin
-        axiov <= 1'b1;
-        if (word1 + word2 + word3 + word4 > 17'b0_1111_1111_1111_1111) begin
-          sum <= word1 + word2 + word3 + word4 + (word1 + word2 + word3 + word4)>>16;
-        end else begin
-          sum <= word1 + word2 + word3 + word4;
-        end
-        if (read_idx < write_idx) begin
-          if (read_count < DATA_SIZE/N - 2) begin
-            read_count <= read_count + 1;
-          end else if (read_count < DATA_SIZE/N-1) begin
-            read_count <= read_count + 1;
-            read_idx <= read_idx + 1;
-          end else begin
-            read_count <= read_count + 1;
-          end
+      if (read_request && read_idx <= write_idx) begin
+         
+        if (cycle_delay_start == 0) begin
+          axiov <= 1'b1;
+          cycle_delay_start <= 1'b1;
+        end else if (read_count < DATA_SIZE/N - 2) begin
+          read_count <= read_count + 1;
+          read_idx <= read_idx + 1;
+        end else if (read_count < DATA_SIZE/N-1) begin
+          read_count <= read_count + 1;
+        end else if (read_count == DATA_SIZE/N-1) begin
+          read_count <= 0;
+          axiov <= read_idx == write_idx ? 0 : axiov;
         end
       end else begin
         axiov <= 0;
-        read_idx <= 0;
-        read_count <= 0;
+        if (axiiv == 1) begin
+          read_idx <= 0;
+          read_count <= 0;
+          cycle_delay_start <= 0;
+        end
       end
     end
   end
